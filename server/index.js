220310c67f95ffebe760a29b7d2ca5ae243fdb02
Brenda Nahomi Quispe Gonzales <adnerbi.1107@gmail.com>
2022-02-16 20:59:02 -0500

    filtro y listado

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
-console.log(process.env);
 
 const path = require('path');
 const express = require('express');
 const app = express();
 const { Client } = require('pg');
-const res = require('express/lib/response');
+const moment = require('moment');
+moment.locale('es-mx');
+
+// const sql = require('sql');
+const formatNumber = require('format-number');
 
 const client = new Client({
   user: process.env.PG_USUARIO,
@@ -17,56 +20,49 @@ const client = new Client({
 
 client.connect();
 
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res);
-});
-
-console.log(__dirname);
-console.log(__filename);
-
 app.set('view engine', 'hbs');
 
 const rutaDePublic = path.join(__dirname, '../public');
-console.log(rutaDePublic);
+
 app.use(express.static(rutaDePublic));
 
-// Logger 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
-
-app.get('', (req, res) => {
-  res.send('Estamos en la ruta raiz!');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about', {
-    nombre: 'Misael',
-    apellido: 'Calvillo Mancilla'
+app.get('/data', (req, res) => {
+  res.render('index', {
+    title: 'Movimientos',
+    method: 'Listar - Filtrar '
   })
 });
 
-app.get('/gastos', (req, res) => {
-  // Obtener los gastos de la base de datos
-  res.render('gastos', {
-    gastos: []
-  });
+app.get('/movimientos', (req, res) => {
+  let { inicio, fin }= req.query
+  let complement= ` as m join "usuarios" as u on m.user_id=u.id `;
+  if(!inicio && !fin){
+    complement+= ''
+  }
+  else{
+    if(!fin || fin===inicio){
+      complement += `WHERE CAST(m.created_at AS Date)='${inicio}'`
+    }
+    else{
+      if(!inicio) 
+        inicio="2000-01-01"
+      complement += `WHERE CAST(m.created_at AS Date)>='${inicio}' AND CAST(m.created_at AS Date)<='${fin}'`;
+    }
+  }
+  
+  client.query(`SELECT m.id, u.nickname, u.name, m.created_at, m.amount FROM "movimientos"${complement};`, (err, postgresRes) => {
+    if (err) {          
+      console.log(err);
+      return res.status(403).send('Estamos trabando para poner en linea esto :('); // temporal
+    } else {
+      let  newData = postgresRes.rows;
+      return res.status(200).json({
+        data: newData
+      });
+    }
+  })  
 });
-
-
-app.post('/gasto', (req, res) => {
-  // Crear gasto en base de datos
-  res.send('Se creo un gasto');
-});
-
-app.listen(3000, () => {
-  console.log('El server acaba de inicial en el puerto 3000');
+const port_server=process.env.PORT_NODE
+app.listen(port_server, () => {
+  console.log(`El server acaba de inicial en el puerto ${port_server}`);
 }); 
-
-
-// CRUD 
-// Create
-// Read 
-// Delete 
-// Update 
